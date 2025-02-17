4ee1277d4be24fcb9169953cc0942194cc1fd066
Laurette <borisiakokouvi@gmail.com>
2025-02-17 01:34:13 +0100

    Ajout de fichiers de configuration, de composants et de tests, ainsi que de fichiers .gitignore pour une meilleure gestion des ressources et des dépendances.

---
+++ resources/views/Chirps/index.blade.php
@@ -0,0 +1,14 @@
+<x-app-layout>
+    <div class="max-w-2xl p-4 mx-auto sm:p-6 lg:p-8">
+        <form method="POST" action="{{ route('chirps.store') }}">
+            @csrf
+            <textarea
+                name="message"
+                placeholder="{{ __('What\'s on your mind?') }}"
+                class="block w-full border-gray-300 rounded-md shadow-sm focus:ring-opacity-50 focus:border-indigo-300 focus:ring focus:ring-indigo-200"
+            >{{ old('message') }}</textarea>
+            <x-input-error :messages="$errors->get('message')" class="mt-2" />
+            <x-primary-button class="mt-4">{{ __('Chirp') }}</x-primary-button>
+        </form>
+    </div>
+</x-app-layout>
